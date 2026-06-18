c385558dbe46ec0f6467d22c7077b6a267b1632a
hussain-najeb <hussainajeb@proton.me>
2026-06-18 04:51:54 +0300

    Small layout changes to the download button and the textfield

--- app/src/main/java/org/foss/fermux/ytdlp/ui/DownloadScreenUi.kt
+++ app/src/main/java/org/foss/fermux/ytdlp/ui/DownloadScreenUi.kt
@@ -3,15 +3,20 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,12 +25,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import org.foss.fermux.R
 import org.foss.fermux.ytdlp.logic.DownloadStatus
 import org.foss.fermux.ytdlp.logic.downloaderLogic
 import org.foss.fermux.ytdlp.logic.fetchingTheMetadata
@@ -69,7 +78,7 @@ fun DownloadContent() {
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(4.dp),
+                .padding(9.dp),
             value = downloadUrl,
             onValueChange = { txt -> downloadUrl = txt },
             label = { Text("Type URL here") },
@@ -81,41 +90,46 @@ fun DownloadContent() {
 
         )
 
-        // https://www.youtube.com/watch?v=ZFSN40r--zk&list=PLpR23XfB8-1UM2BbVMGeLMAQs3oiO5MRt&index=6
-
-
-        Box {
-            FilledTonalButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(70.dp)
-                    .padding(top = 30.dp),
-
-                onClick = {
-                    scope.launch {
-
-                        state = DownloadStatus.Loading
-
-                        try {
-                            val metadata = fetchingTheMetadata(downloadUrl)
-                            state = DownloadStatus.Loaded(metadata)
-                                downloaderLogic(context, downloadUrl) { progress ->
-                                    state = DownloadStatus.Downloading(progress, metadata)
-                                }
-                            state = DownloadStatus.Loaded(metadata)
-                            Log.d("fermux", "state is set to loaded")
-                            Log.d("fermux", "Download succeeded for $downloadUrl")
-                        }catch (e: Exception) {
-                            Log.e("fermux", "Download failed for $downloadUrl", e)
-                            Log.d("fermux", "state failed to get stop the loading bar")
-                        }
-
-                    }
-
-                }) {
-                Text("Download")
-            }
-        }
+        // https://www.youtube.com/watch?v=ZFSN40r--zk&list=PLpR23XfB8-1UM2BbVMGeLMAQs3oiO5MRt&index=6 <-- testing
 
     }
+
+    Box(contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            modifier = Modifier
+                .padding(29.dp)
+                .size(74.dp),
+            contentPadding = PaddingValues(0.dp),
+            shape = RoundedCornerShape(16.dp),
+            onClick = {
+                scope.launch {
+
+                    state = DownloadStatus.Loading
+
+                    try {
+                        val metadata = fetchingTheMetadata(downloadUrl)
+                        state = DownloadStatus.Loaded(metadata)
+                        downloaderLogic(context, downloadUrl) { progress ->
+                            state = DownloadStatus.Downloading(progress, metadata)
+                        }
+                        state = DownloadStatus.Loaded(metadata)
+                        Log.d("fermux", "state is set to loaded")
+                        Log.d("fermux", "Download succeeded for $downloadUrl")
+                    }catch (e: Exception) {
+                        Log.e("fermux", "Download failed for $downloadUrl", e)
+                        Log.d("fermux", "state failed to get stop the loading bar")
+                    }
+
+                }
+
+            } ) {
+            Icon(painter = painterResource(id = R.drawable.baseline_file_download_42),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp))
+        }
+    }
+
 }
